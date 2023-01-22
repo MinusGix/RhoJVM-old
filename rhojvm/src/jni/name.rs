04631a82f7abbfb22cd08536be52c952a657caca
MinusGix <MinusGix@gmail.com>
2023-01-22 15:37:29 -0600

    Add support for more types of JNI encoded names

--- rhojvm/src/jni/name.rs
+++ rhojvm/src/jni/name.rs
@@ -4,63 +4,44 @@
 pub fn make_native_method_name(class_name: &[u8], method_name: &[u8]) -> Vec<u8> {
     let mut result = b"Java_".to_vec();
 
-    escape_name(class_name).extend_vec(&mut result);
+    escape_name(class_name, &mut result);
     result.push(b'_');
-    escape_name(method_name).extend_vec(&mut result);
-    // FIXME: Descriptor for overloded native method
+    escape_name(method_name, &mut result);
+    // FIXME: Descriptor for overloaded native method
 
     result.push(0);
 
     result
 }
-fn escape_name(name: &[u8]) -> EscapeNameIterator<'_> {
-    EscapeNameIterator { name, index: 0 }
-}
 
-enum EscapeRes {
-    Char(u8),
-    TwoChar((u8, u8)),
-}
-impl EscapeRes {
-    fn extend_vec(self, vec: &mut Vec<u8>) {
-        match self {
-            EscapeRes::Char(x) => vec.push(x),
-            EscapeRes::TwoChar((x, y)) => {
-                vec.push(x);
-                vec.push(y);
+fn escape_name(name: &[u8], out: &mut Vec<u8>) {
+    for ch in name {
+        match *ch {
+            b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9' => out.push(*ch),
+            b'/' => out.push(b'_'),
+            b'_' => {
+                out.push(b'_');
+                out.push(b'1');
+            }
+            b';' => {
+                out.push(b'_');
+                out.push(b'2');
+            }
+            b'[' => {
+                out.push(b'_');
+                out.push(b'3');
+            }
+            _ => {
+                // Encode ch as _0xxxx
+                // TODO: This can be done without allocation
+                let val = format!("{ch:04x}");
+                out.push(b'_');
+                out.push(b'0');
+                for v in val.bytes() {
+                    out.push(v);
+                }
             }
         }
-    }
-}
-struct EscapeNameIterator<'a> {
-    name: &'a [u8],
-    index: usize,
-}
-impl<'a> EscapeNameIterator<'a> {
-    fn extend_vec(self, vec: &mut Vec<u8>) {
-        for res in self {
-            res.extend_vec(vec);
-        }
-    }
-}
-impl<'a> Iterator for EscapeNameIterator<'a> {
-    type Item = EscapeRes;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let res = self.name.get(self.index).map(|ch| match *ch {
-            b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9' => EscapeRes::Char(*ch),
-            b'/' => EscapeRes::Char(b'_'),
-            b'_' => EscapeRes::TwoChar((b'_', b'1')),
-            b';' => EscapeRes::TwoChar((b'_', b'2')),
-            b'[' => EscapeRes::TwoChar((b'_', b'3')),
-            _ => todo!("UTF-16 code points aren't yet supported in native method names"),
-        });
-
-        if res.is_some() {
-            self.index += 1;
-        }
-
-        res
     }
 }
 
@@ -75,5 +56,10 @@ mod tests {
             make_native_method_name(b"java/lang/System", b"registerNatives"),
             b"Java_java_lang_System_registerNatives\0"
         );
+
+        assert_eq!(
+            make_native_method_name(b"java/lang/invoke/MethodHandles$Lookup", b"findStatic"),
+            b"Java_java_lang_invoke_MethodHandles_00024Lookup_findStatic\0"
+        )
     }
 }
