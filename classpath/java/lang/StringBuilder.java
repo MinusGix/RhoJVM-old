917ae12d9ba723fc5b245c0f235d10c82c5a7f68
MinusGix <MinusGix@gmail.com>
2023-02-05 06:09:57 -0600

    Add StringBuilder#insert methods

--- classpath/java/lang/StringBuilder.java
+++ classpath/java/lang/StringBuilder.java
@@ -212,7 +212,123 @@ public final class StringBuilder implements Serializable, CharSequence {
         throw new UnsupportedOperationException("TODO: implement subSequence in terms of substring");
     }
 
-    // TODO: Insert
+    public StringBuilder insert(int at, boolean b) {
+        return this.insert(at, String.valueOf(b));
+    }
+
+    public StringBuilder insert(int at, char c) {
+        return this.insert(at, String.valueOf(c));
+    }
+
+    public StringBuilder insert(int at, char[] str) {
+        return this.insert(at, String.valueOf(str));
+    }
+
+    public StringBuilder insert(int at, double d) {
+        return this.insert(at, String.valueOf(d));
+    }
+
+    public StringBuilder insert(int at, float f) {
+        return this.insert(at, String.valueOf(f));
+    }
+
+    public StringBuilder insert(int at, int i) {
+        return this.insert(at, String.valueOf(i));
+    }
+
+    public StringBuilder insert(int at, long l) {
+        return this.insert(at, String.valueOf(l));
+    }
+
+    public StringBuilder insert(int at, Object obj) {
+        return this.insert(at, String.valueOf(obj));
+    }
+
+    public StringBuilder insert(int at, CharSequence v) {
+        if (v == null) {
+            return this.insert(at, "null");
+        } else {
+            return this.insert(at, v, 0, v.length());
+        }
+    }
+
+    public StringBuilder insert(int at, CharSequence v, int start, int end) {
+        if (v == null) {
+            v = "null";
+        }
+
+        if (start < 0) {
+            throw new StringIndexOutOfBoundsException(start);
+        } else if (end < 0) {
+            throw new StringIndexOutOfBoundsException(end);
+        } else if (start > end) {
+            throw new StringIndexOutOfBoundsException(end - start);
+        }
+
+        int length = end - start;
+
+        ensureCapacity(this.usedLength + length);
+
+        // Shift the data after the insertion point
+        System.arraycopy(this.data, at, this.data, at + length, this.usedLength - at);
+        // Copy the data to insert
+        for (int i = 0; i < length; i++) {
+            this.data[at + i] = v.charAt(start + i);
+        }
+
+        this.usedLength += length;
+
+        return this;
+    }
+
+    public StringBuilder insert(int at, char[] v, int start, int length) {
+        if (at < 0 || at > this.usedLength) {
+            throw new StringIndexOutOfBoundsException(at);
+        } else if (start < 0) {
+            throw new StringIndexOutOfBoundsException(start);
+        } else if (length < 0) {
+            throw new StringIndexOutOfBoundsException(length);
+        } else if (start + length > v.length) {
+            throw new StringIndexOutOfBoundsException(start + length);
+        }
+
+        ensureCapacity(this.usedLength + length);
+        // Shift the data after the insertion point
+        System.arraycopy(this.data, at, this.data, at + length, this.usedLength - at);
+        // Copy the data to insert
+        System.arraycopy(v, start, this.data, at, length);
+
+        this.usedLength += length;
+
+        return this;
+    }
+
+    public StringBuilder insert(int at, String v) {
+        if (at < 0 || at > this.usedLength) {
+            throw new StringIndexOutOfBoundsException(at);
+        }
+
+        // Special case
+        if (v == null) {
+            v = "null";
+        }
+
+        int length = v.length();
+
+        ensureCapacity(this.usedLength + length);
+
+        // Shift the data after the insertion point
+        System.arraycopy(this.data, at, this.data, at + length, this.usedLength - at);
+        
+        // Copy the data to insert
+        for (int i = 0; i < length; i++) {
+            this.data[i + at] = v.charAt(i);
+        }
+
+        this.usedLength += length;
+
+        return this;
+    }
 
     // TODO: Reverse
 
